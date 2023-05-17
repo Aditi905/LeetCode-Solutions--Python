0961779c1a7809c53c907dfff3a9bdc444e75577
Aditi <68419381+Aditi905@users.noreply.github.com>
2023-05-17 20:32:01 +0530

    Time: 744 ms (26.30%), Space: 22.1 MB (5.92%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -12,6 +12,26 @@ class Solution:
                     index=i
                     
         return index
+        #for minimum mahattan distance
+#         list1=[]
+#         minimum=1000000
+#         for i in points:
+#             # print(i)
+#             # print(i[0])
+#             if(i[0]==x or i[1]==y):
+#                 list1.append(i)
+#         print(list1)
+#         if len(list1)==0:
+#             return -1
+#         if len(list1)==1:
+#             return 0
+#         else:
+#             for i in range(len(list1)):
+#                 ans=0
+#                 for j in range(i+1,len(list1)):
+#                     ans = (abs(list1[i][0] - list1[j][0]) +abs(list1[i][1] - list1[j][1]))
+#                     minimum=min(minimum,ans)
+#         return minimum
     
     
     
